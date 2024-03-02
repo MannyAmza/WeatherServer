31fc143449ef0f91c9d8f66c0240137e649cb3c9
MannyAmza <mannyamza@gmail.com>
2024-03-01 20:45:27 -0800

    I did some stuff with weather server

---
+++ CountryModel/City.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CountryModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    public int CityId { get; set; }
+
+    [Column(TypeName = "numeric(18, 4)")]
+    public decimal Latitude { get; set; }
+
+    [Column(TypeName = "numeric(18, 4)")]
+    public decimal Longitude { get; set; }
+
+    [Column("CountryID")]
+    [StringLength(10)]
+    public string CountryId { get; set; } = null!;
+
+    [ForeignKey("CityId")]
+    [InverseProperty("City")]
+    public virtual Country CityNavigation { get; set; } = null!;
+}
