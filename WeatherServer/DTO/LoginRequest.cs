8d6e97a629ca7e7fd8b7bac4887e2d72765413ee
MannyAmza <mannyamza@gmail.com>
2024-05-10 07:16:12 -0700

    Removed unnessary files and folders

--- WeatherServer/DTO/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeatherServer.DTO
-{
-    public class LoginRequest
-    {
-        public required string UserName { get; set; }
-        public required string Password { get; set; }
-    }
-}
--- WeatherServer/DTO/LoginResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WeatherServer.DTO
-{
-    public class LoginResult
-    {
-        public bool Success { get; set; }
-        public string? Message {  get; set; }
-        public string? Token { get; set; }
-    }
-}
